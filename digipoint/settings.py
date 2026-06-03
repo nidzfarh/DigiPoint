e141923f6e7016cf759b9120a3ab445b615be631
nidzfarh <nidzfarheen@gmail.com>
2026-06-04 00:21:58 +0530

    fixed admin

--- digipoint/settings.py
+++ digipoint/settings.py
@@ -16,7 +16,9 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1'
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://digipoint.onrender.com'
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -30,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,7 +95,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'store' / 'static',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
